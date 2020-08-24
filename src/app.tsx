d7947c6299d65a0681a8d87e8ace1317f1eb5600
Kaki <kaki.so2011@gmail.com>
2020-08-25 04:35:40 +0900

    convert to typescript + deleted unncessary files

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box } from '@material-ui/core';
+
+const App: React.FC = () => {
+  return (
+    <Box>da log is here! hehe</Box>
+  );
+};
+
+export default App;
